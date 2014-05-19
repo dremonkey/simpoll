4dd983dd799660125d45591a800edf1d4ff4b0e6
dremonkey <a.deutmeyer@gmail.com>
2014-05-19 13:22:57 -0700

    added text parser for poll questions

--- web/src/app/pages/controllers/polls.ctrl.js
+++ web/src/app/pages/controllers/polls.ctrl.js
@@ -2,14 +2,54 @@
 
 angular.module('dapoll.pages.controllers')
   .controller('PollsCtrl', function ($scope, geolocation) {
+
+    // ## Local Variables
+    var questions = []; // array to hold the poll questions
+    var punctuation = {
+      page: /-{5,}/gi
+    };
+
+    var cleanText = function (arr) {
+      for (var i = arr.length - 1; i >= 0; i--) {
+        arr[i] = arr[i].trim();
+      };
+
+      return arr;
+    };
+
+    var submit = function () {
+      var poll = {};
+
+      // Create the Poll
+      poll.name = $scope.polls.name;
+      poll.lat = $scope.coords.lat;
+      poll.lon = $scope.coords.lon;
+
+      // Create each of the questions
+
+      // Create answers
+    };
+
+    // ## Parse Body
+    $scope.$watch('polls.body', function (newText, oldText) {
+      // console.log(newText, '\n\n' + oldText);
+
+      if (newText) {
+        pages = newText.split(punctuation.page);
+      }
+    });
+
+    // ## Get Location
     geolocation.getLocation().then(function(data){
       $scope.coords.lat = data.coords.latitude
-      $scope.coords.long = data.coords.longitude;
+      $scope.coords.lon = data.coords.longitude;
       $scope.coords.acc = data.coords.accuracy;
 
       console.log($scope.coords);
     });
 
-    // ## Scope Variables
+    // ## Setup Scope Variables
+    $scope.polls = {};
     $scope.coords = {};
+    $scope.submit = submit;
   });
